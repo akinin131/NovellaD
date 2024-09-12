cf9a7f43b2db638c4b09dbbd216ce0ded0b06463
ilya <ilya.akinin@list.ru>
2024-09-12 23:24:54 +0300

    Добавил проверку подписка

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -10,10 +10,10 @@ import com.github.kotlintelegrambot.entities.ChatId
 import com.github.kotlintelegrambot.entities.ChatMember
 import com.github.kotlintelegrambot.logging.LogLevel
 import com.github.kotlintelegrambot.types.TelegramBotResult
-import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import kotlinx.coroutines.supervisorScope
+import kotlinx.coroutines.*
 
 fun main() {
     val myBot = MyBot()
@@ -22,6 +22,8 @@ fun main() {
 
 class MyBot {
     private var currentState: BotState = InitialStateLableOne()
+    private val processedCallbackQueryIds = mutableSetOf<String>()
+    private val botScope = CoroutineScope(Dispatchers.Default)
 
     fun createBot(): Bot {
         return bot {
@@ -31,36 +33,40 @@ class MyBot {
             dispatch {
                 text {
                     val text = update.message?.text
-
                     if (text != null) {
                         val chatId = ChatId.fromId(update.message!!.chat.id)
                         val userId = update.message!!.from?.id
                         val chatIdQroup = ChatId.fromId(-1002229947613)
 
                         val result = bot.getChatMember(chatIdQroup, userId!!.toLong())
-
                         println("result111 = $result")
-                        // Переместите вызов addDocumentToCollection сюда
-                        GlobalScope.launch {
+
+                        botScope.launch {
                             handleTextAndUpdateState(chatId, text, bot, result)
                         }
                     }
 
                     callbackQuery("check_subscription") {
+                        val callbackId = callbackQuery.id
                         val chatId = callbackQuery.message?.chat?.id
                         val userId = callbackQuery.from.id
 
-                        if (chatId != null) {
-                            val chatIdGroup = ChatId.fromId(-1002229947613L)
+                        if (chatId != null && !processedCallbackQueryIds.contains(callbackId)) {
+                            processedCallbackQueryIds.add(callbackId)
+                            println("Processing callback query with ID: $callbackId")
 
+                            val chatIdGroup = ChatId.fromId(-1002229947613L)
                             val result = bot.getChatMember(chatIdGroup, userId.toLong())
 
                             result.fold(
                                 {
                                     val chatMember = it
-                                    if (chatMember.status == "member" || chatMember.status == "administrator" || chatMember.status == "creator") {
+                                    if (chatMember.status in listOf("member", "administrator", "creator")) {
                                         bot.sendMessage(ChatId.fromId(chatId), "✅ Подписка подтверждена! Спасибо!")
                                         bot.deleteMessage(ChatId.fromId(chatId), callbackQuery.message?.messageId!!)
+                                        botScope.launch {
+                                            handleTextAndUpdateState(ChatId.fromId(chatId), "✅ Подписка подтверждена! Спасибо!", bot, result)
+                                        }
                                     } else {
                                         bot.sendMessage(ChatId.fromId(chatId), "❌ Вы не подписаны на группу. Пожалуйста, подпишитесь и попробуйте снова.")
                                     }
@@ -72,6 +78,7 @@ class MyBot {
                         }
                     }
                 }
+
                 initializeFirebase()
 
                 preCheckoutQuery {
@@ -85,19 +92,17 @@ class MyBot {
                     }
                 }
 
-                // Обработка успешных платежей
                 message {
                     val successfulPayment = this.message.successfulPayment
                     if (successfulPayment != null) {
                         runBlocking {
-                            handleSuccessfulPayment( bot, message, ChatId.fromId(update.message!!.chat.id))
+                            handleSuccessfulPayment(bot, message, ChatId.fromId(update.message!!.chat.id))
                         }
                     }
                 }
             }
         }
     }
-
 
     private suspend fun handleTextAndUpdateState(
         chatId: ChatId,
@@ -106,10 +111,10 @@ class MyBot {
         result: TelegramBotResult<ChatMember>
     ) {
         supervisorScope {
-                val newState = BotStateFactory.getState(text)
-                if (newState != null) {
-                    currentState = newState
-                }
+            val newState = BotStateFactory.getState(text)
+            if (newState != null) {
+                currentState = newState
+            }
             println(newState)
             currentState.handleText(chatId, text, bot, result)
         }
